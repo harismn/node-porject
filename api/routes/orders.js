754bed7b1bfe3a479d570b1888f3ee8f6fffd82c
harismn <harismannug@gmail.com>
2018-07-24 23:35:03 +0700

    continue to database

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,10 +8,14 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/:orderId', (req, res, next) => {
+router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
         message: 'Order was created',
-        orderId: req.params.orderId
+        order: order
     })
 })
 
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const productRoutes = require('./api/routes/product')
 const orderRoutes = require('./api/routes/orders')
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 // Route which sould handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Acces-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept, Authorization" 
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Acces-Control-Allow-Method', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+    next();
+})
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
@@ -22,7 +38,8 @@ app.use((error, req, res, next) => {
         error: {
             message: error.message
         }
-    })
-})
+    });
+});
+
 
 module.exports = app;
